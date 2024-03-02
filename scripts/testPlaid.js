1db0a7307b71eed85d768dbe8bc8978943990732
Benedict Gattas <gattasb@gmail.com>
2024-03-02 11:14:03 +0000

    Rename plaid fetch file

--- scripts/testPlaid.js
+++ scripts/testPlaid.js
@@ -1,6 +1,8 @@
 require('dotenv').config()
 
-import { fetchTransactions } from '../lib/fetch';(async () => {
+import { fetchTransactions } from '../lib/plaidFetch';
+
+(async () => {
   const res = await fetchTransactions()
   console.log('Transactions fetch successful!')
   console.log(res)
